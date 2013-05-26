a2c6697a162c22fcb02ca4fd35df75231f7d91cd
元兒～ <yuan817@moztw.org>
2013-05-26 15:14:41 +0800

    修改內定參數

--- src/tw/edu/chu/csie/e_learning/Config.java
+++ src/tw/edu/chu/csie/e_learning/Config.java
@@ -2,6 +2,18 @@ package tw.edu.chu.csie.e_learning;
 
 public class Config {
 	/**
+	 * 資料庫登入資訊
+	 */
+	//資料庫連接網址
+	public static final String SQL_URL = "localhost";
+	//資料庫連接埠
+	public static final String SQL_PORT = "";
+	//資料庫登入帳號
+	public static final String SQL_LOGIN_ID = "";
+	//資料庫登入密碼
+	public static final String SQL_LOGIN_PASSWORD = "";
+	
+	/**
 	 * 無帳號登入
 	 */
 	//是否啟用自動填入預設帳號密碼
@@ -11,5 +23,5 @@ public class Config {
 	//預設登入的密碼
 	public static final String DEFAULT_LOGIN_PASSWORD = "123456";
 	//是否啟用自動登入
-	public static final boolean AUTO_NO_ID_LOGIN = false;
+	public static final boolean AUTO_NO_ID_LOGIN = true;
 }
