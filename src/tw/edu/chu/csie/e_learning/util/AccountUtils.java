4476eecdfaa3c4b82e38df9ebed88c4827cbd998
元兒～ <yuan817@moztw.org>
2013-06-03 15:26:04 +0800

    騰寫登入的程式

--- src/tw/edu/chu/csie/e_learning/util/AccountUtils.java
+++ src/tw/edu/chu/csie/e_learning/util/AccountUtils.java
@@ -10,5 +10,45 @@
 package tw.edu.chu.csie.e_learning.util;
 
 public class AccountUtils {
-
+	/**
+	 * 
+	 */
+	private static boolean isLogined;
+	private static String loginedId;
+	
+	
+	/**
+	 * 是否是已登入狀態
+	 */
+	public boolean islogin(){
+		return isLogined;
+	}
+	
+	/**
+	 * 察看已登入的ID
+	 */
+	public String getLoginId() {
+		return loginedId;
+	}
+	/**
+	 * 登入帳號
+	 * @param inputLoginId 使用者輸入的ID
+	 * @param inputLoginPasswd 使用者輸入的密碼
+	 * @return 是否登入成功
+	 */
+	public boolean loginUser(String inputLoginId, String inputLoginPasswd){
+		//TODO 騰入登入動作
+		
+		return false;
+	}
+	/**
+	 * 登出帳號
+	 * @return 是否登出成功
+	 */
+	public boolean logoutUser(){
+		isLogined = false;
+		//TODO 清除登入資訊
+		
+		return true;
+	}
 }
