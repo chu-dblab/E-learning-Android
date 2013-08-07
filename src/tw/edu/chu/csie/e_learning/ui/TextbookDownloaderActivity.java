1e0ab0a1825e7978590ca4bd6f64b2f8e231234e
TomLin100 <3011850@gmail.com>
2013-08-08 01:25:46 +0800

    將不需要的檔案刪除

--- src/tw/edu/chu/csie/e_learning/ui/TextbookDownloaderActivity.java
+++ src/tw/edu/chu/csie/e_learning/ui/TextbookDownloaderActivity.java
@@ -1,8 +1,11 @@
 package tw.edu.chu.csie.e_learning.ui;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.io.*;
+import java.net.*;
 
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import tw.edu.chu.csie.e_learning.R;
 
 /**
@@ -12,5 +15,51 @@ public class TextbookDownloaderActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_textbook_downloader);
+    }
+    /**
+     * 
+     * @author Tony 2013/08/06
+     * 
+     *
+     */
+    @SuppressWarnings("unused")
+    public class DownloadTextBookTask extends AsyncTask<String, Integer, Void>
+    {
+    	
+		private HttpURLConnection url_con;
+    	private URL http_url;
+    	private InputStream is;
+    	private FileOutputStream output2SDCard;
+    	
+    	@Override
+    	protected Void doInBackground(String... params) {
+    		// TODO 自動產生的方法 Stub
+    		return null;
+    	}
+
+    	@Override
+    	protected void onCancelled() {
+    		// TODO 自動產生的方法 Stub
+    		super.onCancelled();
+    	}
+
+    	@Override
+    	protected void onPostExecute(Void result) {
+    		// TODO 自動產生的方法 Stub
+    		super.onPostExecute(result);
+    	}
+
+    	@Override
+    	protected void onPreExecute() {
+    		// TODO 自動產生的方法 Stub
+    		super.onPreExecute();
+    	}
+
+    	@Override
+    	protected void onProgressUpdate(Integer... values) {
+    		// TODO 自動產生的方法 Stub
+    		super.onProgressUpdate(values);
+    	}
+    	
     }
 }
--- src/tw/edu/chu/csie/e_learning/ui/UserLoginActivity.java
+++ src/tw/edu/chu/csie/e_learning/ui/UserLoginActivity.java
@@ -1,10 +1,10 @@
 /*
- * 無所不在學習架構與學習導引機制
+ * �⊥�銝摮貊��嗆��飛蝧�撘���
  * A Hybrid Ubiquitous Learning Framework and its Navigation Support Mechanism
  * 
  * FileName:	UserLoginActivity.java
  *
- * Description: 程式進入點，這是使用者的登入畫面
+ * Description: 蝔��脣暺��雿輻���餃�恍
  * 
  */
 package tw.edu.chu.csie.e_learning.ui;
@@ -104,12 +104,12 @@ public class UserLoginActivity extends Activity {
 					}
 				});
 		
-		//自動填入預設帳號密碼
+		//�芸�憛怠�身撣唾�撖Ⅳ
 		if(Config.AUTO_FILL_LOGIN){
 			mIdView.setText(Config.DEFAULT_LOGIN_ID);
 			mPasswordView.setText(Config.DEFAULT_LOGIN_PASSWORD);
 		}
-		//自動登入
+		//�芸��餃
 		if(Config.AUTO_NO_ID_LOGIN) attemptLogin();
 	}
 
@@ -135,7 +135,7 @@ public class UserLoginActivity extends Activity {
 	}
 
 	/**
-	 * 驗證是否輸入正確，若無誤就登入
+	 * 撽��臬頛詨甇�Ⅱ嚗�∟炊撠梁��
 	 * 
 	 * Attempts to sign in or register the account specified by the login form.
 	 * If there are form errors (invalid email, missing fields, etc.), the
@@ -194,7 +194,7 @@ public class UserLoginActivity extends Activity {
 	}
 
 	/**
-	 * 顯示登入中畫面
+	 * 憿舐內�餃銝剔��
 	 * 
 	 * Shows the progress UI and hides the login form.
 	 */
@@ -237,7 +237,7 @@ public class UserLoginActivity extends Activity {
 	}
 
 	/**
-	 * 進行登入的動作
+	 * �脰��餃��雿�
 	 * 
 	 * Represents an asynchronous login/registration task used to authenticate
 	 * the user.
