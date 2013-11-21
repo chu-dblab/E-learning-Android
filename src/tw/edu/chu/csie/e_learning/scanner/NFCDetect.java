563b3df8a1ab7df2d16f487a530711e7fe6ab971
~kobayashi(); <3011850@gmail.com>
2013-11-21 12:47:15 +0800

    將NFC的程式碼改成不用繼承Activity

--- src/tw/edu/chu/csie/e_learning/scanner/NFCDetect.java
+++ src/tw/edu/chu/csie/e_learning/scanner/NFCDetect.java
@@ -3,124 +3,83 @@ package tw.edu.chu.csie.e_learning.scanner;
 import tw.edu.chu.csie.e_learning.ui.MaterialActivity;
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentFilter.MalformedMimeTypeException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.os.Bundle;
-import android.os.Parcelable;
+import android.nfc.NfcManager;
+import android.nfc.Tag;
+import android.nfc.tech.NfcA;
+import android.util.Log;
 
-@SuppressWarnings("unused")
-public class NFCDetect extends Activity 
+public class NFCDetect
 {
-	private PendingIntent gNfcPendingIntent;
-	private NfcAdapter gNfcAdapter;
-	private IntentFilter[] gNdefExchangeFilters;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		// 註冊讓該Activity負責處理所有接收到的NFC Intents。
-		gNfcPendingIntent = PendingIntent.getActivity(this, 0,
-		        // 指定該Activity為應用程式中的最上層Activity
-		        new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-		
-		// 建立要處理的Intent Filter負責處理來自Tag的資料。
-		IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
-		try {
-		    ndefDetected.addDataType("text/plain");
-		} catch (MalformedMimeTypeException e) { }
-		gNdefExchangeFilters = new IntentFilter[] { ndefDetected };
-	}
-	
-	
-
-	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		gNfcAdapter.disableForegroundDispatch(this);
-	}
-
-
-
-	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-	    // 處理由Android系統送出應用程式處理的intent filter內容
-	    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-	        // 取得NdefMessage
-	        NdefMessage[] messages = getNdefMessages(getIntent());
-	        // 取得實際的內容
-	        byte[] payload = messages[0].getRecords()[0].getPayload();
-	         String url = new String(payload);
-	        // 往下送出該intent給其他的處理對象
-	        setIntent(new Intent()); 
-	    }
-	    // 啟動前景模式支持Nfc intent處理
-	    enableNdefExchangeMode();
-	}
-	
-	private void enableNdefExchangeMode() {
-
-	    // 讓NfcAdapter啟動能夠在前景模式下進行intent filter的dispatch。
-	    gNfcAdapter.enableForegroundDispatch(this, gNfcPendingIntent, gNdefExchangeFilters, null);
-	}
-	
-	public NdefMessage[] getNdefMessages(Intent intent)
+	private Context baseContext;
+	private NfcManager manager;
+	private NfcAdapter adapter;
+	private IntentFilter nfc_tech;
+	private IntentFilter[] nfcFilter;
+	private PendingIntent nfcPendingIntent;
+	private Intent nfc_intent;
+	private String[][] tech_list;
+	private Activity act;
+	public NFCDetect(Context context)
 	{
-	    // Parse the intent
-	    NdefMessage[] msgs = null;
-	    String action = intent.getAction();
-	    // 識別目前的action為何
-	    if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action) || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) 
-	    {
-	        // 取得parcelabelarrry的資料
-	        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-	        // 取出的內容如果不為null，將parcelable轉成ndefmessage
-
-	        if (rawMsgs != null) {
-	            msgs = new NdefMessage[rawMsgs.length];
-	            for (int i = 0; i < rawMsgs.length; i++) {
-	                msgs[i] = (NdefMessage) rawMsgs[i];
-	            }
-	        } 
-	       else {
-	            // Unknown tag type
-	            byte[] empty = new byte[] {};
-	            NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
-	            NdefMessage msg = new NdefMessage(new NdefRecord[] { record });
-	            msgs = new NdefMessage[] { msg };
-	        }
-	     } 
-	    else {
-	       // Log.d(TAG, "Unknown intent.");
-	        finish();
-	    }
-	    return msgs;
-	}
-
-	@Override
-	protected void onNewIntent(Intent intent) {
-		// TODO Auto-generated method stub
-		super.onNewIntent(intent);
-	    // 覆寫該Intent用於補捉如果有新的Intent進入時，可以觸發的事件任務。
-	    // NDEF exchange mode
-	    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
-			NdefMessage[] msgs = getNdefMessages(intent);
-	    }
+		this.baseContext = context;
 	}
 	
-	public void searchFileInDatabase(final NdefMessage msg)
+	public void InitialDetect()
 	{
-		String str = new String(msg.getRecords()[0].getPayload());
-		//發送Intent給webview
-		Intent startLearning = new Intent(NFCDetect.this,MaterialActivity.class);
-		startLearning.putExtra("ID", str);
-		startActivityForResult(startLearning,1);
+		manager = (NfcManager)act.getSystemService(Context.NFC_SERVICE);
+		adapter = manager.getDefaultAdapter();
+		nfc_intent = new Intent(this.baseContext,getClass());
+		nfcPendingIntent = PendingIntent.getActivity(this.baseContext, 0, nfc_intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+		nfc_tech = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+		nfcFilter = new IntentFilter[]{nfc_tech};
+		tech_list = new String[][]{new String[]{NfcA.class.getName()}};
+	}
+	
+	public void callMaterial(Intent intent)
+	{
+		String action = intent.getAction();
+		Log.d("Tag_type",action);
+		if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action))
+		{
+			Tag tagContent = intent.getParcelableExtra(action);
+		}
+		else if(NfcAdapter.ACTION_TECH_DISCOVERED.equals(action))
+		{
+			
+		}
+		else if(NfcAdapter.ACTION_TAG_DISCOVERED.equals(action))
+		{
+			
+		}
+		else
+		{
+			//do nothing
+		}
+	}
+	
+	private String readTagContent(Tag tag)
+	{
+		return null;
+	}
+	
+	private void sentIntentToMaterial(String targetID)
+	{
+		Intent learningTarget = new Intent(this.baseContext,MaterialActivity.class);
+		learningTarget.putExtra("materialID", targetID);
+		act.startActivityForResult(learningTarget,1);
+	}
+	
+	public void setForegroundDispatchOnPause(Activity activity)
+	{
+		adapter.disableForegroundDispatch(activity);
+	}
+	
+	public void setForegroundDispatchOnResume(Activity activity)
+	{
+		adapter.enableForegroundDispatch(activity, nfcPendingIntent, nfcFilter, tech_list);
 	}
 }
