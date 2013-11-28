da77afe9fd46e8d761a9f09ab82b56e746ae86c3
~kobayashi(); <3011850@gmail.com>
2013-11-29 00:36:35 +0800

    修正解壓縮的Bug

--- src/tw/edu/chu/csie/e_learning/util/FileUtils.java
+++ src/tw/edu/chu/csie/e_learning/util/FileUtils.java
@@ -8,6 +8,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.util.*;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 import tw.edu.chu.csie.e_learning.config.Config;
@@ -19,6 +20,7 @@ public class FileUtils
 {
 	private File BasicSDPath;  //SD卡的根目錄（會依照Android版本不同而有所變化）
 	private Context BasicInternalPath;		//內部儲存裝置的根目錄(預設是/data/data/[package.name]/files/)
+	private ZipEntry entry;
 	
 	public FileUtils() 
 	{
@@ -101,51 +103,39 @@ public class FileUtils
 	
 	public void decompressFile() throws IOException
 	{
-		//打開要解壓縮的檔案
-		FileInputStream fin = new FileInputStream(getPath()+Config.ZIP_FILE_NAME_OF_MATERIAL);
-		BufferedInputStream in = new BufferedInputStream(fin);
-		ZipInputStream zipInput = new ZipInputStream(in);
-
-		//取得壓縮檔的目的資料夾
-		ZipEntry next = zipInput.getNextEntry();
-		Log.d("Name", next.getName());
-		File tmp = new File(getPath()+next.getName());
-		String unzipDirectory = getPath();
-		if(!next.isDirectory()) //如果壓縮檔內的目的資料夾不是目錄的話
+		//
+		InputStream is = null;
+       BufferedInputStream bi = null;
+       BufferedOutputStream bo = null;
+		File zipFile = new File(getPath()+Config.ZIP_FILE_NAME_OF_MATERIAL);
+		ZipFile unzip = new ZipFile(zipFile);
+		Enumeration<? extends ZipEntry> entryEnum  = unzip.entries();
+		
+		//如果壓縮黨內還有目錄或檔案的話
+		while(entryEnum.hasMoreElements())
 		{
-			//先判斷上一層資料夾是否存在，若不存在則先建立資料夾，再解壓縮檔案			
-			File save = tmp.getParentFile();
-			if(!save.exists()) save.mkdirs();
-			
-			//解壓縮
-			outputToStorage(tmp,zipInput);
-			
-			//將解壓縮完成的zip檔刪除
-			//File remove = new File(getPath()+Config.FILE_NAME_OF_MATERIAL);
-			//remove.delete();
+			entry = entryEnum.nextElement();
+			File outFile = new File(getPath(), entry.getName());
+          if (entry.isDirectory()) 
+            {
+              Log.d("decompress", "Add a folder: " + outFile.getAbsolutePath());
+              outFile.mkdir();
+              if (!outFile.exists()) 
+                {
+                  Log.e("decompress","Can't create this path: "+ outFile.getAbsolutePath());
+                  return;
+                }
+            }
+          else 
+            {
+                Log.d("decompress", "Add a file: " + outFile.getAbsolutePath());
+                is = unzip.getInputStream(entry);
+                bi = new BufferedInputStream(is);
+                bo = new BufferedOutputStream(new FileOutputStream(outFile));
+                int data = 0;
+                while ((data = bi.read()) != -1) bo.write(data);
+                bo.flush();
+            }
 		}
-		else outputToStorage(tmp,zipInput);
-	}
-	/**
-	 * outputToStorage
-	 * 將解壓縮後的資料寫入檔案之後儲存
-	 * @param fobj
-	 * @param zIn
-	 * @throws IOException
-	 */
-	private void outputToStorage(File fobj,ZipInputStream zIn) throws IOException
-	{
-		//開啟解壓縮要寫入的檔案
-		FileOutputStream output = new FileOutputStream(fobj);
-		
-		//以byte讀取解壓縮後的資料
-		int data = 0;
-		byte[] buf = new byte[1024];
-		
-		//寫入檔案，然後關閉所有檔案相關串流
-		while((data=zIn.read(buf)) != -1) output.write(buf,0,data);
-		output.flush();
-		output.close();
-		zIn.close();
 	}
 }
