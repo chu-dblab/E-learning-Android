ade5e4df06412703d9fbef98c332690049f342ab
JennyTsai <lupjk123@gmail.com>
2013-12-19 10:06:22 +0800

    刪掉Study Table禮的屬性：回答時間、題目編號、答題對錯，所以剩下3個屬性：進入標的時間、離開標的時間、有無正確到學習點

--- src/tw/edu/chu/csie/e_learning/provider/ClientDBHelper.java
+++ src/tw/edu/chu/csie/e_learning/provider/ClientDBHelper.java
@@ -23,7 +23,7 @@ public class ClientDBHelper extends SQLiteOpenHelper {
 				"CREATE TABLE chu_target ( TID INTEGER unsigned NOT NULL, MapID INTEGER unsigned NOT NULL, Map_Url varchar(150) NOT NULL, MaterialID INTEGER unsigned NOT NULL, Material_Url varchar(150) NOT NULL, PRIMARY KEY (TID));";
 	//學習關係資料表
 	private static final String sstudy = 
-				"CREATE TABLE chu_study ( TID INTEGER unsigned NOT NULL, UID varchar(30) NOT NULL, QID INTEGER unsigned default NULL, Answer varchar(5) default NULL, Answer_Time varchar(10) default NULL, In_TargetTime datetime NOT NULL, Out_TargetTime datetime default NULL, TCheck varchar(5) NOT NULL, FOREIGN KEY (UID) REFERENCES user, FOREIGN KEY (TID) REFERENCES target, PRIMARY KEY(UID,TID));";
+				"CREATE TABLE chu_study ( TID INTEGER unsigned NOT NULL, UID varchar(30) NOT NULL, In_TargetTime datetime NOT NULL, Out_TargetTime datetime default NULL, TCheck varchar(5) NOT NULL, FOREIGN KEY (UID) REFERENCES user, FOREIGN KEY (TID) REFERENCES target, PRIMARY KEY(UID,TID));";
 	//輔助類建立時運行該方法
 	@Override
 	public void onCreate(SQLiteDatabase db) {
