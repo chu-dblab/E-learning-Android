64d4a5ec24ffda24fce797726b340075f2a72d17
JennyTsai <lupjk123@gmail.com>
2013-12-20 11:56:54 +0800

    增加target表格屬性：教材編號、教材URL

--- src/tw/edu/chu/csie/e_learning/provider/ClientDBHelper.java
+++ src/tw/edu/chu/csie/e_learning/provider/ClientDBHelper.java
@@ -20,7 +20,7 @@ public class ClientDBHelper extends SQLiteOpenHelper {
 				"CREATE TABLE chu_user ( UID varchar(30) NOT NULL, UNickname varchar(20) DEFAULT NULL, ULogged_code varchar(32) DEFAULT NULL, In_Learn_Time varchar(50) NOT NULL, PRIMARY KEY (UID));"; 
 	//標的資料表
 	private static final String tar = 
-				"CREATE TABLE chu_target ( TID INTEGER unsigned NOT NULL, MapID INTEGER unsigned NOT NULL, Map_Url varchar(150) NOT NULL, PRIMARY KEY (TID));";
+				"CREATE TABLE chu_target ( TID INTEGER unsigned NOT NULL, MapID INTEGER unsigned NOT NULL, MaterialID INTEGER unsigned NOT NULL, Map_Url varchar(150) NOT NULL, Material_Url varchar(150) NOT NULL, PRIMARY KEY (TID));";
 	//學習關係資料表
 	private static final String sstudy = 
 				"CREATE TABLE chu_study ( TID INTEGER unsigned NOT NULL, UID varchar(30) NOT NULL, In_TargetTime datetime NOT NULL, Out_TargetTime datetime default NULL, TCheck varchar(5) NOT NULL, FOREIGN KEY (UID) REFERENCES user, FOREIGN KEY (TID) REFERENCES target, PRIMARY KEY(UID,TID));";
